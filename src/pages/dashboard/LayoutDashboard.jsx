ccfe81ecb0fe05a440aec63a50618b684372218c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-19 13:33:32 -0400

    update

--- src/pages/dashboard/LayoutDashboard.jsx
+++ src/pages/dashboard/LayoutDashboard.jsx
@@ -1,11 +1,22 @@
+import { Button } from 'antd'
+import Cookies from 'js-cookie'
 import { Link, Outlet } from 'react-router-dom'
 
 const LayoutDashboard = () => {
+  const handleLogout = () => {
+    Cookies.remove('token')
+    Cookies.remove('name')
+    Cookies.remove('role')
+    window.location.href = '/'
+  }
   return (
     <>
       <div className=''>
         <Link to='/dashboard'>Dashboard</Link>
         <Link to='/dashboard/profile'>Profile</Link>
+        <Button onClick={handleLogout} type='primary'>
+          Log Out
+        </Button>
       </div>
       <Outlet />
     </>
