c45b832eec24a2542bc74d905bf930dd414092c2
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-19 15:14:16 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,26 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          dependencies: [
+            '@ant-design/icons',
+            '@reduxjs/toolkit',
+            'antd',
+            'axios',
+            'js-cookie',
+            'react',
+            'react-dom',
+            'react-query',
+            'react-redux',
+            'react-router-dom',
+            'styled-components',
+          ],
+        },
+      },
+    },
+  },
 })
