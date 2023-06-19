90e7b7c4d5d414fe865a004a7d69bb0b253d9bca
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-19 10:28:50 -0400

    update

---
+++ features/users/userSlice.js
@@ -0,0 +1,63 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { customFetch } from '../../lib/axios/customFetch'
+
+const initialState = {
+  name: '',
+  lastName: '',
+  email: '',
+  isLoading: false,
+}
+export const usersThunk = createAsyncThunk(
+  'users/usersThunk',
+  async (_, thunkAPI) => {
+    try {
+      const response = await customFetch('')
+
+      return response.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data)
+    }
+  }
+)
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    // createFunction: (state, { payload }) => {
+    //   console.log('function call')
+    // },
+    getStateValues: (state, { payload }) => {
+      const { name, value } = payload
+
+      state[name] = value
+    },
+    clearState: (state) => {
+      state.name = ''
+      state.lastName = ''
+      state.email = ''
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(usersThunk.pending, (state, { payload }) => {
+        console.log('promise pending')
+        console.log(payload)
+        state.isLoading = true
+      })
+      .addCase(usersThunk.fulfilled, (state, { payload }) => {
+        console.log('promise fulfilled')
+        console.log(payload)
+        state.isLoading = false
+      })
+      .addCase(usersThunk.rejected, (state, { payload }) => {
+        console.log('promise rejected')
+        console.log(payload)
+        state.isLoading = false
+      })
+  },
+})
+export const { createFunction, getStateValues, clearState } = usersSlice.actions
+
+export default usersSlice.reducer
--- store.js
+++ store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import sampleSlice from './features/samples/sampleSlice'
+import userSlice from './features/users/userSlice'
 
 const store = configureStore({
   reducer: {
     sample: sampleSlice,
+    user: userSlice,
   },
 })
 
