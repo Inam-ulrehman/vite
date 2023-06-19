950273469bdfec1e3d7a9e03cdae398a94a23b72
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-19 13:07:13 -0400

    update

--- features/users/userSlice.js
+++ features/users/userSlice.js
@@ -5,7 +5,7 @@ const initialState = {
   name: Cookies.get('name') ? Cookies.get('name') : '',
   lastName: '',
   email: '',
-  // isMember: Cookies.get('token') ? true : false,
+  // isMember: Cookies.get('token') ? true : false, test
   isMember: true,
   role: Cookies.get('role') ? Cookies.get('role') : 'user',
   isLoading: false,
