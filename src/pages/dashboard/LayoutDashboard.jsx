86487d9c75d5453f34fcf88335cf8035ef5a7d14
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-20 09:27:55 -0400

    update admin logic

--- src/pages/dashboard/LayoutDashboard.jsx
+++ src/pages/dashboard/LayoutDashboard.jsx
@@ -1,6 +1,7 @@
 import { Button } from 'antd'
 import Cookies from 'js-cookie'
 import { Link, Outlet } from 'react-router-dom'
+import { styled } from 'styled-components'
 
 const LayoutDashboard = () => {
   const handleLogout = () => {
@@ -10,17 +11,24 @@ const LayoutDashboard = () => {
     window.location.href = '/'
   }
   return (
-    <>
+    <Wrapper>
       <div className=''>
         <Link to='/dashboard'>Dashboard</Link>
         <Link to='/dashboard/profile'>Profile</Link>
+        <Link to='/dashboard/admin'>Admin</Link>
+        <Link to='/dashboard/admin/users'>Users</Link>
         <Button onClick={handleLogout} type='primary'>
           Log Out
         </Button>
       </div>
       <Outlet />
-    </>
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.div`
+  a {
+    margin-right: 10px;
+  }
+`
 export default LayoutDashboard
--- src/route.jsx
+++ src/route.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Dashboard from './pages/dashboard/page'
-import ProtectedRoute from './components/ProtectedRoute'
+import ProtectedRoute from './components/singleComponent/ProtectedRoute'
 import ErrorPage from './pages/errorPage'
 import Layout from './layout'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -11,6 +11,10 @@ import Login from './pages/user/login/page'
 import Register from './pages/user/register/page'
 import Recover from './pages/user/recover/page'
 import RecoverPassword from './pages/user/recoverpassword/page'
+import Admin from './pages/dashboard/admin/page'
+import ProtectedAdminRoute from './components/singleComponent/ProtectedAdminRoute'
+import AdminUsers from './pages/dashboard/admin/users/page'
+import LayoutAdminDashboard from './pages/dashboard/admin/LayoutAdminDashboard'
 
 const RoutesConfig = () => {
   return (
@@ -35,11 +39,24 @@ const RoutesConfig = () => {
           >
             <Route index element={<Dashboard />} />
             <Route path='/dashboard/profile' element={<Profile />} />
+
+            {/* ================>>>>> Protected Admin Dashboard Start  */}
+            <Route
+              path='/dashboard/admin'
+              element={
+                <ProtectedAdminRoute>
+                  <LayoutAdminDashboard />
+                </ProtectedAdminRoute>
+              }
+            >
+              <Route index element={<Admin />} />
+              <Route path='/dashboard/admin/users' element={<AdminUsers />} />
+            </Route>
+            {/* ================>>>>> Protected Admin Dashboard End  */}
           </Route>
           {/* ================>>>>> Protected Dashboard End  */}
-
-          {/* ================>>>>> Layout End  */}
         </Route>
+        {/* ================>>>>> Layout End  */}
         {/* ================>>>>> Error Page  */}
         <Route path='*' element={<ErrorPage />} />
       </Routes>
