7e17f4960ec2ddfc1bf423c903d5d92600066a37
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-27 08:00:59 -0400

    update ui

--- src/pages/dashboard/admin/users/tableComponent.jsx
+++ src/pages/dashboard/admin/users/tableComponent.jsx
@@ -2,14 +2,6 @@ import { Button, Space, Table, Tooltip } from 'antd'
 import { UserOutlined, CrownOutlined } from '@ant-design/icons'
 import { useSelector } from 'react-redux'
 const columns = [
-  // {
-  //   title: 'Name',
-  //   dataIndex: 'name',
-  //   key: 'name',
-  //   render: (text) => <a>{text}</a>,
-  //   ellipsis: true,
-  //   width: 150,
-  // },
   {
     title: 'Name',
     dataIndex: ['name', 'lastName'], // Array of field names
@@ -64,6 +56,8 @@ const columns = [
     title: 'Role',
     key: 'role',
     dataIndex: 'role',
+    align: 'center',
+    width: 65,
     render: (role) => {
       const icon =
         role === 'admin' ? (
@@ -77,30 +71,31 @@ const columns = [
         )
       return icon
     },
-    width: 65,
   },
   {
     title: 'Status',
     dataIndex: 'active',
     key: 'active',
+    align: 'center',
+    width: 80,
     render: (_, record) => {
       const { active } = record
       const color = active ? 'green' : 'red'
       const text = active ? 'Active' : 'Inactive'
       return <span style={{ color }}>{text}</span>
     },
-    width: 80,
   },
   {
     title: 'Action',
     key: 'action',
+    align: 'center',
+    width: 80,
     render: (_, record) => (
       // console.log(record),
       <Space size='middle'>
         <Button type='primary'>Edit</Button>
       </Space>
     ),
-    width: 80,
   },
 ]
 
