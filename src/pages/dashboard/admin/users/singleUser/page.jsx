167cae8e26f4df0a726f4874dea50f917cbb7ced
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-27 09:59:04 -0400

    update

--- src/pages/dashboard/admin/users/singleUser/page.jsx
+++ src/pages/dashboard/admin/users/singleUser/page.jsx
@@ -1,5 +1,14 @@
+import { useParams } from 'react-router-dom'
+
 const AdminSingleUser = () => {
-  return <div>SingleUser</div>
+  const { id } = useParams()
+  console.log(id)
+  return (
+    <div>
+      SingleUser Id
+      <h1>{id}</h1>
+    </div>
+  )
 }
 
 export default AdminSingleUser
